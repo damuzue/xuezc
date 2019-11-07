6c65d3c455de26a2ff9e41a07773c1caea0758ed
xuezc <993303011@qq.com>
2019-11-07 09:34:10 +0800

    查询结果修改

--- 作业/查询结果.py
+++ 作业/查询结果.py
@@ -3,9 +3,9 @@ from settings import dbParams1
 
 conn = pymysql.Connect(**dbParams1)
 cursor = conn.cursor()
-username = 'xzc'
-# sql = "select * from user"
-sql = "select username,password from user where username='{}'".format(username)
+# username = 'xzc'
+sql = "select * from user"
+# sql = "select username,password from user where username='{}'".format(username)
 result = cursor.execute(sql)
 data = cursor.fetchone()
 while data:
